db3c66c63374255b1083b1e3ad1f0cc73fb7e1f9
xia1990 <18016074089@163.com>
2020-11-30 14:35:58 +0800

    Update export_jenkins_log.py

--- export_jenkins_log.py
+++ export_jenkins_log.py
@@ -8,7 +8,7 @@ jenkin_url="http://jenkins.mot.com"
 username="gaoyx9"
 password="gyx050400??"
 job_name="QZB30.Q4_borneo-retail_userdebug_mq-2020-q4_r-qsm2020_test-keys_continuous"
-job_number="742"
+job_number=742
 
 server=jenkins.Jenkins(jenkins_url, username=username, password=password)
 #得到指定job控制台输出的log信息息
